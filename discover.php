30de7740f9817b9a5aa63cd0531aa1d3749cb02b
WilliamSCastro <william.sargisson.castro@gmail.com>
2025-01-14 00:12:34 +0100

    finalized integration between login and discover

--- discover.php
+++ discover.php
@@ -11,7 +11,7 @@ if (!isset($_SESSION['user'])) {
 }
 
 // Store loggedUser Object
-$loggedUser = searchUserInDatabase("*", "users", $_SESSION['user']["user_ID"]);
+$loggedUser = searchUserInDatabase("*", "users", $_SESSION['user']);
 
 
 function searchUserInDatabase($whatYouWant, $whereYouWant, $userYouWant) {
