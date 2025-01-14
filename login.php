4b181df3fcc4ec53954770c9277c3668792b5781
Unai Muñoz <unaimunoz2024@gmail.com>
2025-01-14 17:37:03 +0100

    Delete style.css and Update styles.css

--- login.php
+++ login.php
@@ -85,7 +85,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IETinder</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="styles.css">
     <script src="index.js"></script>
 </head>
 <body class="body-login">
